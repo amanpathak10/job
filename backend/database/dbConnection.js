9fa765772ce4947b7094426d8c954edabde0356b
amanpathak10 <maheshpathak9857@gmail.com>
2026-04-18 10:35:57 +0000

    removed hardcoded db name

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -4,10 +4,7 @@ dotenv.config()
 
 //Database connection here!
  const dbConnection  = ()=>{
-    mongoose.connect(process.env.DB_URL,{
-       dbName: "Job_Portal"
-
-    }).then(()=>{ //agar connect ho jaye toh!
+    mongoose.connect(process.env.DB_URL).then(()=>{ //agar connect ho jaye toh!
        console.log("MongoDB Connected Sucessfully !")
     }).catch((error)=>{
         console.log(`Failed to connect ${error}`) //warna error de do console me!
